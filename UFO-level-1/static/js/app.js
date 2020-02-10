cf779317b6d22cfa345160a30eb78c87f601ddb6
Emma Pang <emma.kpang@gmail.com>
2020-02-10 14:11:32 -0500

    console logs filtered data when button is clicked

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,21 +6,6 @@ var tbody = d3.select("tbody");
 // Select the button
 var button = d3.select("#filter-btn");
 
-// console.log(data);
-
-// data.forEach(function(ufoSighting) {
-//   console.log(ufoSighting);
-//   });
-
-
-
-// Step 2:  Use d3 to append one table row `tr` for each weather report object
-// Don't worry about adding cells or text yet, just try appending the `tr` elements.
-// data.forEach(function(ufoSighting) {
-//   console.log(ufoSighting);
-//   var row = tbody.append("tr");
-// });
-
 
 // append table with UFO sighting data from data.js
 data.forEach((ufoSighting) => {
@@ -31,8 +16,21 @@ data.forEach((ufoSighting) => {
     });
   });
 
-  // button function
-  button.on("click", function() {
+//   // button function - it works!
+//   button.on("click", function() {
+
+//     // Select the input element and get the raw HTML node
+//     var inputElement = d3.select("#datetime");
+  
+//     // Get the value property of the input element
+//     var inputValue = inputElement.property("value");
+  
+//     console.log(inputValue);
+  
+
+//   });
+
+button.on("click", function() {
 
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
@@ -40,8 +38,10 @@ data.forEach((ufoSighting) => {
     // Get the value property of the input element
     var inputValue = inputElement.property("value");
   
-    console.log(inputValue);
+    // console.log(inputValue);
+    // console.log(data);
   
-
-  });
+    var filteredData = data.filter(sighting => sighting.datetime === inputValue);
   
+    console.log(filteredData);
+});
