dd84799ee1841e385c5433713c6ac30b1ead95e5
Emma Pang <emma.kpang@gmail.com>
2020-02-10 13:25:14 -0500

    added js to append table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,28 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+// console.log(data);
+
+// data.forEach(function(ufoSighting) {
+//   console.log(ufoSighting);
+//   });
+
+
+
+// Step 2:  Use d3 to append one table row `tr` for each weather report object
+// Don't worry about adding cells or text yet, just try appending the `tr` elements.
+// data.forEach(function(ufoSighting) {
+//   console.log(ufoSighting);
+//   var row = tbody.append("tr");
+// });
+
+
+// BONUS: Refactor to use Arrow Functions!
+data.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
